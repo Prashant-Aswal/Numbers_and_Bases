ed58bef9a66c1ca0ad280091ead956c77523bd1d
Prashant Aswal <prashant.aswal89@gmail.com>
2025-06-04 21:52:55 +0530

    Added Any subdecimal Base to Decimal converter in new Algo

---
+++ Any_Subdecimal_B_To_Dec_Alt.py
@@ -0,0 +1,59 @@
+def num_check(num,base):
+    i = 0
+    snum = str(num)
+    for n in snum:
+        if int(snum[i]) >= base:
+            return 'Invalid Number'
+            break
+        else:
+            i = i + 1
+
+
+def anybtodec(X,B):
+    diff = B - 10
+    p = 0
+    val = []
+    N = int(X / 10)
+    val.append(N * diff * (B ** p))
+    p = p + 1
+    while N > 0:
+        N = int(N / 10)
+        val.append(N * diff * (B ** p))
+        p = p + 1
+    ans = X + sum(val)
+    return ans
+
+
+while True:
+    print('\n')
+    base = input('Enter the Base (2 to 10) of your Number: ')
+    try:
+        base = int(base)
+        if base < 2 or base > 10:
+            print('Invalid Input')
+            continue
+        else:
+            break
+    except:
+        print('Invalid Input')
+        continue
+
+while True:
+    print('\n')
+    num = input('Enter any Integer in your selected Base: ')
+    try:
+        num = int(num)
+        chk = num_check(num,base)
+        if chk == 'Invalid Number':
+            print('Entered Integer is not from the selected Base')
+            continue
+        else:
+            break
+    except:
+        print('Invalid Input')
+        continue
+
+result = anybtodec(num,base)
+print('\n')
+print(num,'in Base',base,'is',result,'in Decimal')
+print('\n')
