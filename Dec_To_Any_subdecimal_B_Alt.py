2a50bffc3d5a770a7eb97f7460c1e502bee7897f
Prashant Aswal <prashant.aswal89@gmail.com>
2025-06-04 20:02:44 +0530

    Added Dec to Any subddcimal converter based on new Algo

---
+++ Dec_To_Any_subdecimal_B_Alt.py
@@ -0,0 +1,39 @@
+def dectoanyb(X,B):
+    diff = 10 - B
+    p = 0
+    val = []
+    N = int(X / B)
+    val.append(N * diff * (10 ** p))
+    p = p + 1
+    while N > 0:
+        N = int(N / B)
+        val.append(N * diff * (10 ** p))
+        p = p + 1
+    ans = X + sum(val)
+    return ans
+
+
+while True:
+    X = input('Enter any Positive Integer: ')
+    try:
+        X = int(X)
+        break
+    except:
+        print('Invalid Entry')
+        continue
+
+while True:
+    B = input('Enter any Base (2 to 10): ')
+    try:
+        B = int(B)
+        if B < 2 or B > 10:
+            print('Invalid Entry')
+            continue
+        else:
+            break
+    except:
+        print('Invalid Entry')
+        continue
+
+result = dectoanyb(X,B)
+print(result)
